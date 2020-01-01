19f1aee7f1220a1708c7b6295ae2ce5266db205a
Divij Kanwar <divijkanwar19@gmail.com>
2020-01-02 02:36:13 +0530

    virtual voice assistant (Scooby)
    Scooby is a voice assistant.

---
+++ voice_assistant(scooby).py
@@ -0,0 +1,109 @@
+import pyttsx3 
+import speech_recognition as sr 
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Mr Kanwar")
+    
+    elif hour>=12 and hour<16:
+        speak("Good Afternoon! Mr Kanwar ")
+    
+    elif hour>=16 and hour<20:
+        speak("Good Evening! Mr Kanwar")
+
+    else:
+        speak("Good Night! Mr Kanwar")
+    
+    speak("I am Scooby, How may i assist you ?")
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing....")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Could you repeat please....")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('mymail@gmail.com', 'password')
+    server.sendmail('myemail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+        
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            music_dir = 'C:\\DivijKanwar\\songs\\songs'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\divij\\AppData\\Local\\Programs\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "yourotherEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend harry bhai. I am not able to send this email")    
